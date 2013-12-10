5acab7c5713556e839b0c371dd8ecf91f8776ef4
Troy Gaul <tgaul@mac.com>
2013-12-10 15:36:11 -0600

    Updated copyright date.

--- InfColorPicker/InfColorIndicatorView.m
+++ InfColorPicker/InfColorIndicatorView.m
@@ -5,7 +5,7 @@
 //
 //  Created by Troy Gaul on 8/10/10.
 //
-//  Copyright (c) 2011 InfinitApps LLC: http://infinitapps.com
+//  Copyright (c) 2011-2013 InfinitApps LLC: http://infinitapps.com
 //	Some rights reserved: http://opensource.org/licenses/MIT
 //
 //==============================================================================
--- PickerSamplePhone/Classes/PickerSamplePhoneViewController.m
+++ PickerSamplePhone/Classes/PickerSamplePhoneViewController.m
@@ -5,7 +5,7 @@
 //
 //  Created by Troy Gaul on 8/12/10.
 //
-//  Copyright (c) 2011 InfinitApps LLC: http://infinitapps.com
+//  Copyright (c) 2011-2013 InfinitApps LLC: http://infinitapps.com
 //	Some rights reserved: http://opensource.org/licenses/MIT
 //
 //==============================================================================
